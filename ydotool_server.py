9d20e23a6ea32a3d8256305e5fc7f878458d677e
yile <kangshan9988@gmail.com>
2025-08-17 23:32:06 +0800

    feat: noting work on the kodi commands

--- ydotool_server.py
+++ ydotool_server.py
@@ -254,14 +254,13 @@ async def handle_message(websocket):
                     print(f"command:{cmd_to_exec}")
                     if cmd_to_exec:
                         if os.name == 'posix':  # macOS/Linux
-                            display_env = os.environ.copy()
-                            display_env['DISPLAY'] = ':0'
-                            subprocess.run(cmd_to_exec, check=True, shell=True, env=display_env)
-                            # subprocess.run([shlex.quote(cmd_to_exec)])
+                            # display_env = os.environ.copy()
+                            # display_env['DISPLAY'] = ':0'
+                            subprocess.run(cmd_to_exec, check=True, shell=True)
+                            response["message"] = f"Executed: {cmd_to_exec}"
                         else:
                             response["status"] = "error"
                             response["message"] = "This command is only supported on Linux/macOS."
-                        response["message"] = f"Executed: {cmd_to_exec}"
                     else:
                         response["status"] = "error"
                         response["message"] = "Command not specified."
